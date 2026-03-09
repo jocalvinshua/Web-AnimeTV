7cccf5f05ef4b5d7543680787555bc83c663f9f4
Jocalvin <calvinjoshua37@gmail.com>
2026-03-09 20:40:49 +0700

    Updated Github Icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { Search, X } from 'lucide-react'; // Menggunakan lucide untuk ikon
+import { Search, X, } from 'lucide-react'; // Menggunakan lucide untuk ikon
 import { Link, useNavigate } from 'react-router-dom';
+import githubIcon from "../assets/github-icon.svg"
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +24,6 @@ export default function Navbar() {
     <>
       <nav className="bg-navbar sticky top-0 h-[70px] w-full px-4 md:px-12 lg:px-24 flex items-center justify-between z-50 backdrop-blur-md text-bright shadow-lg border-b border-white/5 transition-all mb-6">
         
-        {/* Logo Section */}
         <div className="flex items-center gap-8">
           <Link to="/" className="flex items-center gap-2 group">
             <h1 className="text-xl font-extrabold tracking-tight text-bright">
@@ -36,6 +36,22 @@ export default function Navbar() {
         </div>
 
         <div className="flex items-center gap-4 flex-1 justify-end max-w-xl">
+          {/* Github Logo */}
+          <a 
+            href="https://github.com/jocalvinshua/Web-AnimeTV"
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="p-2 transition-all duration-300 hover:scale-110 active:scale-95 flex items-center"
+            title="View Source on GitHub"
+          >
+            <svg 
+              viewBox="0 0 24 24" 
+              className="w-6 h-6 fill-current text-muted hover:text-bright transition-colors"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+            </svg>
+          </a>
           <button 
             onClick={() => setIsSearchOpen(true)} 
             className="bg-transparent p-2 transition-all duration-300 ease-out hover:scale-110 active:scale-95 text-bright hover:text-primary"
